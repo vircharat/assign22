8354a9c20603c08fcf3112624d06a026331899e8
Chandan.M <Chandan.M@Valtech.com>
2022-11-08 18:15:06 +0530

    day 2 end product

---
+++ RestaurantManagement/RestaurantDAL/Migrations/20221107090803_rest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantDAL.Migrations
+{
+    public partial class rest1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "EmpPhone",
+                table: "tbl_Employee",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EmpPhone",
+                table: "tbl_Employee",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
